ecdfa32bc9964b232dfeedffaa617b40c8364070
Kay Nelson <kayxn23@gmail.com>
2018-08-13 16:04:46 -0700

    checks for unique index

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,13 +1,25 @@
-# all letters
-letters = %W(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i j k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t t u u u u v v w w x y y z )
-
-# range
-max = letters.length
-min = 0
-
-# index used
 
 
-# generate random index
-index = Random.rand(min..max)
-puts "#{letters[index]}"
+def draw_letters
+  # all letters
+  letters = %W(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i j k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t t u u u u v v w w x y y z )
+
+  # range
+  max = letters.length
+  min = 0
+
+  # index used
+  indices_used= []
+
+  # generate random index
+  index = Random.rand(min..max)
+
+  # check if index is in indices_used array , if true generate new random index
+  while indices_used.include?(index)
+    index = Random.rand(min..max)
+  end
+  # if not is not in indices used, add it there
+  indices_used << index
+end
+
+draw_letters
