669c3c3c7ab9f77b4c96088f35e4bc4e6f83749d
Kay Nelson <kayxn23@gmail.com>
2018-08-14 16:48:36 -0700

    wave 1 and wave 2 passed tests

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,17 +1,13 @@
-
-# outputs a nill for one of the letters
-# check for if index = Random.rand(min..max) includes max? it's returning nill at some point, check using lower range
-
-# 1. write condition that checks if index is nill? if it give me another index
-
-
 def draw_letters
 
   # all letters
   letters = %W(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i j k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t t u u u u v v w w x y y z )
-
+  # convert to upcase
+  letters = letters.map do |letter|
+    letter.upcase
+  end
   # range
-  max = letters.length
+  max = letters.length - 1
   min = 0
 
   # index used
@@ -37,31 +33,28 @@ def draw_letters
   return ten_letters
 end
 
-letters_in_hand = draw_letters
-
-puts draw_letters
-puts "Make a word from all ten of those letters!"
-input = gets.chomp!
-
-# it's not retuning true when the input is exactly as letters_in_hand
 def uses_available_letters? (input, letters_in_hand)
+  result = []
+  final_results = []
   input.each_char do |letter|
-    print letter
+    # check if letter exists in letters_in_han
+    if letters_in_hand.include?(letter)
+      result << true
+    else
+      # user used a letter that does not exists
+      result << false
+    end
+  end
+  final_results << result.all? { |value| value == true}
+
+  input.each_char do |letter|
     # check if letter exists in letters_in_hand
     if letters_in_hand.include?(letter)
-      # if it exits, delete
       letters_in_hand.delete(letter)
     else
       # user used a letter that does not exists
-      return puts "false"
+      final_results << false
     end
   end
-  # check if all letters in letters_in_hand were used
-  if letters_in_hand.empty?
-    return puts "true"
-  else
-    return puts "false"
-  end
+  final_results.all? { |value| value == true}
 end
-
-print uses_available_letters?(input, letters_in_hand)
